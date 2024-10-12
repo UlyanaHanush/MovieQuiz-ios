cc306ed620503eaa7a30ca5ce64a7cbe0196cbe5
UlyanaHanush <ulyana.hanush@gmail.com>
2024-10-13 00:05:29 +0300

    created AlertPresenter

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import UIKit
 
-class AlertPresenter {
+final class AlertPresenter {
+    private weak var viewController: UIViewController?
     
+    init(viewController: UIViewController?) {
+        self.viewController = viewController
+    }
+    
+    // приватный метод для показа результатов раунда квиза
+    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+    func show(quiz result: AlertModel) {
+        let alert = UIAlertController(
+            title: result.title, // заголовок всплывающего окна
+            message: result.message, // текст во всплывающем окне
+            preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
+        
+        // создаём для алерта кнопку с действием
+        // в замыкании пишем, что должно происходить при нажатии на кнопку
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            result.completion()
+
+        }
+        
+        // добавляем в алерт кнопку
+        alert.addAction(action)
+        
+        // показываем всплывающее окно
+        viewController?.present(alert, animated: true, completion: nil)
+    }
 }
---
+++ MovieQuiz/Services/AlertProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  AlertProtocol.swift
+//  MovieQuiz
+//
+//  Created by ulyana on 12.10.24.
+//
+
+import Foundation
+protocol AlertProtocol {
+    func show(quiz result: AlertModel)
+}
